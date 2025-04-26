e2949f5f3204d16d929816883ec9e893df1ceda4
Daniil <danil456t@gmail.com>
2025-04-26 23:56:28 +0300

    Changed GammaProtonClassifier11, added scheduler to train_evaluate

--- tunka_nn.py
+++ tunka_nn.py
@@ -90,7 +90,8 @@ def evaluate(model, val_loader, criterion):
     return running_loss / len(val_loader.dataset)
 
 
-def train_evaluate(model, criterion, optimizer, epochs, train_loader, test_loader, graph: bool = True, graph_every: int = 1):
+def train_evaluate(model, criterion, optimizer, epochs, train_loader, test_loader, 
+                   graph: bool = True, graph_every: int = 1, scheduler = None):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
     model.to(device)
@@ -102,9 +103,15 @@ def train_evaluate(model, criterion, optimizer, epochs, train_loader, test_loade
         # train_losses.append(train(model, train_loader, criterion, optimizer))
         train(model, train_loader, criterion, optimizer)
 
+        train_loss = evaluate(model, train_loader, criterion)
+        val_loss = evaluate(model, test_loader, criterion)
+
+        if scheduler is not None:
+            scheduler.step(val_loss)
+
         if graph:
-            train_losses.append(evaluate(model, train_loader, criterion))
-            val_losses.append(evaluate(model, test_loader, criterion))
+            train_losses.append(train_loss)
+            val_losses.append(val_loss)
 
             if (i+1) % graph_every == 0:
                 clear_output(True)
@@ -283,17 +290,17 @@ class GammaProtonClassifier10(nn.Module):
         self.ln = nn.LayerNorm(8 * 5 * 5 + 9)
 
         self.fc = nn.Sequential(
-            nn.Linear(8 * 5 * 5 + 9, 256),
+            nn.Linear(8 * 5 * 5 + 9, 512),
+            nn.LayerNorm(512),
+            nn.Tanh(),
+            # nn.Dropout(p=0.2),
+
+            nn.Linear(512, 256),
             nn.LayerNorm(256),
             nn.Tanh(),
             # nn.Dropout(p=0.2),
 
-            nn.Linear(256, 128),
-            nn.LayerNorm(128),
-            nn.Tanh(),
-            # nn.Dropout(p=0.2),
-
-            nn.Linear(128, 2)
+            nn.Linear(256, 2)
         )
 
     def forward(self, image, features):
@@ -321,43 +328,43 @@ class GammaProtonClassifier11(nn.Module):
         super(GammaProtonClassifier11, self).__init__()
 
         self.pad1 = nn.ConstantPad2d(1, -1.0)
-        self.conv1 = nn.Conv2d(4, 8, kernel_size=3, padding=0)
+        self.conv1 = nn.Conv2d(4, 16, kernel_size=3, padding=0)
         self.relu1 = nn.ReLU()
-        # self.dropout1 = nn.Dropout2d(p=0.2)
+        self.dropout1 = nn.Dropout2d(p=0.2)
 
-        self.conv2 = nn.Conv2d(8, 8, kernel_size=3, padding=1) # [batch_size, 8, 5, 5]
+        self.conv2 = nn.Conv2d(16, 16, kernel_size=3, padding=1) # [batch_size, 8, 5, 5]
         self.relu2 = nn.ReLU()
-        # self.dropout2 = nn.Dropout2d(p=0.2)
+        self.dropout2 = nn.Dropout2d(p=0.2)
 
         self.skip = nn.Identity()
 
-        self.ln = nn.LayerNorm(8 * 5 * 5 + 9)
+        self.ln = nn.LayerNorm(16 * 5 * 5 + 9)
 
         self.fc = nn.Sequential(
-            nn.Linear(8 * 5 * 5 + 9, 256),
+            nn.Linear(16 * 5 * 5 + 9, 512),
+            nn.LayerNorm(512),
+            nn.Tanh(),
+            nn.Dropout(p=0.2),
+
+            nn.Linear(512, 256),
             nn.LayerNorm(256),
             nn.Tanh(),
-            # nn.Dropout(p=0.2),
+            nn.Dropout(p=0.2),
 
-            nn.Linear(256, 128),
-            nn.LayerNorm(128),
-            nn.Tanh(),
-            # nn.Dropout(p=0.2),
-
-            nn.Linear(128, 2)
+            nn.Linear(256, 2)
         )
 
     def forward(self, image, features):
         x_img = self.pad1(image)
         x_img = self.conv1(x_img)  # [batch_size, 16, 5, 5]
         x_img = self.relu1(x_img)
-        # x_img = self.dropout1(x_img)
+        x_img = self.dropout1(x_img)
 
         identify = self.skip(x_img)
 
         x_img = self.conv2(x_img)
         x_img = self.relu2(x_img)
-        # x_img = self.dropout2(x_img)
+        x_img = self.dropout2(x_img)
 
         x_img = x_img + identify
 
